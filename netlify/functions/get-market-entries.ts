ce846cc4ba1e820017fc42d40fa42a751d438d1a
NielzosFilms <nielzosfilms@gmail.com>
2022-04-15 14:25:25 +0200

    get-market-entries

--- netlify/functions/get-market-entries.ts
+++ netlify/functions/get-market-entries.ts
@@ -1,31 +1,45 @@
 import {Handler} from "@netlify/functions";
 import {Client} from "@notionhq/client";
 import {environment} from "../../src/environments/environment";
+import {MarketEntry} from "../../src/app/database-services/market-entry-type";
+
+const columns = {
+  price: 'Price (Diamonds)',
+  purchase: 'Purchase',
+  date: 'Date',
+  amount: 'Amount (Items)',
+  item: 'Item',
+  bulk: 'Bulk'
+}
 
 export const handler: Handler = async (event, context) => {
-  try {
-    const client = new Client({
-      auth: environment.notion_key
-    });
+  const client = new Client({
+    auth: environment.notion_key
+  });
 
-    const result = await client.databases.query({
-      database_id: environment.notion_database_id_entries
-    });
+  const result = await client.databases.query({
+    database_id: environment.notion_database_id_entries
+  });
 
-    return {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      statusCode: 200,
-      body: JSON.stringify(result),
-    }
-  } catch (e) {
-    return {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      statusCode: 500,
-      body: JSON.stringify({error: e})
-    }
+  return {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    statusCode: 200,
+    body: JSON.stringify(mapResult(result)),
   }
+}
+
+function mapResult(result: any): MarketEntry[] {
+  return result.result.map((row: any): MarketEntry => ({
+    id: row.id,
+    item_id: row[columns.item].relation[0].id,
+    // item: null,
+    amount_of_diamonds: row[columns.price].number,
+    amount: row[columns.amount].number,
+    transaction_date: row[columns.date].date.start,
+    bulk: row[columns.bulk].checkbox,
+    was_purchase: row[columns.purchase].checkbox,
+    created_by: row["Property"].created_by.name,
+  }))
 }
--- src/app/database-services/market-entry-type.ts
+++ src/app/database-services/market-entry-type.ts
@@ -3,14 +3,14 @@ import {Item} from "./item-type";
 export interface MarketEntry {
   id: string;
   item_id: string;
-  item: Item;
+  item?: Item;
   amount_of_diamonds: number;
   amount: number;
   transaction_date: Date;
   bulk: boolean;
   was_purchase: boolean;
   created_by: string;
-  created_at: string;
+  created_at?: string;
 }
 
 export interface MarketEntryInput {
