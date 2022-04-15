602111b47afedf8deb03821255c638ea39e00d3f
NielzosFilms <nielzosfilms@gmail.com>
2022-04-15 15:32:14 +0200

    get-market-entries

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,3 @@ export const environment = {
   notion_database_id_items: '6920a5d761134f828b64e7d313a5bfc3',
   notion_database_id_entries: 'c0a94cf992834eb88a0ae79629bbef1c'
 };
-
-//echo "export const environment = {   production: true,   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3dmZmcnR1Y3lrZG9xZWdleGNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc2MDE0NDUsImV4cCI6MTk2MzE3NzQ0NX0.PtsUHQlzmqrFaC9HgzHgpj4tqj6Q4PwEKx58s4Uk5i8',   supabaseUrl: 'https://hwvffrtucykdoqegexcj.supabase.co'};" > src/environments/environment.prod.ts && npm run build
