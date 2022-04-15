c8ef692ef2265b9d7c0fb5bdd044c9c185e4c4d2
NielzosFilms <nielzosfilms@gmail.com>
2022-04-15 15:31:41 +0200

    get-market-entries

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {ProfileService} from "../database-services/profile.service";
 
 @Component({
   selector: 'app-team',
@@ -7,14 +6,7 @@ import {ProfileService} from "../database-services/profile.service";
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent {
-  public loading = false;
-  public profiles: string[] = [];
 
-  constructor(private profileService: ProfileService) {
-    this.loading = true;
-    profileService.getProfiles().then(profiles => {
-      this.profiles = profiles;
-      this.loading = false;
-    });
+  constructor() {
   }
 }
